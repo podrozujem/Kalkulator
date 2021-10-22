e6a1f7a61fd1984a58be1cd032bef8fb9adffa86
Lara Gutman <laragutman27@gmail.com>
2021-10-22 15:34:16 +0200

    Initial read loop

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -1,10 +1,24 @@
 package kalkulator;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner scanner; //prethodno iportujemo taj paket da bi mogli koristiti tu vrstu promenljive 
+		scanner = new Scanner(System.in);
+		
+		//scanner.nextLine(); //vraca povratnu vrednost 
+		
+		String s = scanner.nextLine(); 
+		System.out.println(s);
+		
+		do {
+			//kod koji se izvrsava 
+			s = scanner.nextLine(); 
+		}while(!s.equals("kraj"));
 	}
 
 }

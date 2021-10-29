199abab6369153c05ec27b8adde80a872092ea1c
Lara Gutman <laragutman27@gmail.com>
2021-10-29 14:11:33 +0200

    ADD feature addition

--- src/kalkulator/Main.java
+++ src/kalkulator/Main.java
@@ -35,10 +35,12 @@ public class Main {
 			int x = Integer.parseInt(pars[0]);	 // prima string kao parametar i prebacuje u int (kao atoi)
 			int y = Integer.parseInt(pars[2]);
 			if(pars[1].equals("+")) {
-				System.out.println(x+y);
+				addition(x,y);
 			}
 			
 		}while(!s.equals("kraj"));
 	}
-
+	public static void addition(int x, int y) {
+		System.out.println(x+y); 
+	}
 }
